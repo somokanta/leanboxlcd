03f59a83ed6b20495c038ec29a1c4c69e724c999
Hardik_Patel_12 <hardik.patel@iksula.com>
2017-03-22 17:12:17 +0530

    menu migration

--- sites/all/themes/leanbox/js/leanbox_custom.js
+++ sites/all/themes/leanbox/js/leanbox_custom.js
@@ -6,4 +6,15 @@ jQuery(document).ready(function($){
     	$(".date-padding").closest(".views-exposed-widget").addClass("date_field_parent");
     }
 
+    jQuery(".explore_menu").click(function(){
+    	$(".menu_overlay").toggle();
+    	$(".menu_dropdown").stop(true,true).slideToggle();
+        jQuery('li.expanded.dropdown a').removeAttr('data-toggle');
+    });
+
+     $(".menu_overlay").click(function(){
+    	$(".menu_overlay").toggle();
+    	$(".menu_dropdown").stop(true,true).slideUp();
+    });
+
 });
