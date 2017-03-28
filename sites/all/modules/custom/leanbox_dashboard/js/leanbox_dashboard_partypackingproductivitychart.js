192faee0797431c959c83cb47d0afb1161724af0
Hardik_Patel_12 <hardik.patel@iksula.com>
2017-03-28 12:14:56 +0530

    productivity chart

--- sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_partypackingproductivitychart.js
+++ sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_partypackingproductivitychart.js
@@ -6,17 +6,16 @@
 
 (function ($) {
 
-	Drupal.behaviors.leanbox_dashboard_picking = {
+	Drupal.behaviors.leanbox_dashboard_party_packing_productivity = {
 		attach: function (context, settings) {
-
-			var picking_data = Drupal.settings.leanbox_dashboard.picking;
 			var area_definition = Drupal.settings.leanbox_dashboard.area_definition;
+			var party_pack_productivity_data = Drupal.settings.leanbox_dashboard.party_packing_productivity;
 			google.charts.load("current", {packages: ['gauge','corechart', 'bar']});
 			google.charts.setOnLoadCallback(function () {
-				drawpickingChart(picking_data, area_definition);
+				drawpartypackingproductivityChart(party_pack_productivity_data);
 			});
 
-			$(".picking-submit").click(function (e) {
+			$(".party-packing-productivity-submit").click(function (e) {
 
 				e.preventDefault();
 			  var start_date = $(this).parent().prev().prev().find("input[name='start_date[date]']").val();
@@ -26,60 +25,50 @@
 						type: "POST",
 						cache: false, //for Chrome and IE8
 						url: "/chart-daterange-filter",
-						data: {start_date: start_date, end_date: end_date, activity_type: 'picking'},
+						data: {start_date: start_date, end_date: end_date, activity_type: 'party_packing_productivity'},
 						success: function (response) {
-							
 							var area_definition = Drupal.settings.leanbox_dashboard.area_definition;
 							google.charts.setOnLoadCallback(function () {
-								drawpickingChart(response, area_definition);
+								drawpartypackingproductivityChart(response);
 							});
 						},
 					});
 				}
+				
 			});
 
-			function drawpickingChart(picking_data, area_definition) {
-				var data = google.visualization.arrayToDataTable(picking_data);
+			function drawpartypackingproductivityChart(party_pack_data) {
 
+	      var data = google.visualization.arrayToDataTable(party_pack_data);
+		
 				var view = new google.visualization.DataView(data);
 				view.setColumns([0,
 					1, {calc: "stringify",
 						sourceColumn: 1,
 						type: "string",
 						role: "annotation"},
-					2, {calc: "stringify",
-						sourceColumn: 2,
-						type: "string",
-						role: "annotation"},
-					3, {calc: "stringify",
-						sourceColumn: 3,
-						type: "string",
-						role: "annotation"},
+					
 				]);
 				var options = {
 					width: area_definition.width,
 					height: area_definition.height,
 					title: '',
 					legend: {position: 'right', maxLines: 3},
-            
 					bar: {groupWidth: '40%'},
 					chartArea: {left: area_definition.ch_left, top: area_definition.ch_top, width: area_definition.ch_width, height: area_definition.ch_height},
 					series: {
-						0: {color: '#f39c12'},
-						1: {color: '#e74c3c'},
-						2: {color: '#27ae60'},
+			
 					},
-					isStacked: true,		
 					vAxis: {
 						minValue: 0,
 						maxValue: 7,
-						title: 'Picking Activity'
+						title: 'Party Packing Productivity'
 					},
 					hAxis: {
-						title: 'Pricelist Group'
+						title: 'Party Packing Productivity'
 					},
 				};
-				var chart = new google.visualization.ColumnChart(document.getElementById('picking_div'));
+				var chart = new google.visualization.ColumnChart(document.getElementById("party_packing_productivity_div"));
 				chart.draw(view, options);
 			}
 		}
