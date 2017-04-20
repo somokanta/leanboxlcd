9244118af1d62a10da3f976b89850e97bb403429
dhirendra mishra <dhirenmishra10@gmail.com>
2017-04-20 15:50:20 +0530

    round oercentage in graph

--- sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_pickingchart.js
+++ sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_pickingchart.js
@@ -48,21 +48,21 @@
 				view.setColumns([0,
 					1, {
 						calc: function (dt, row) {
-							return dt.getValue(row, 1) + ' (' + formatPercent.formatValue(dt.getValue(row, 1) / (dt.getValue(row, 1) + dt.getValue(row, 2) + dt.getValue(row, 3))) + ')';
+							return dt.getValue(row, 1) + ' (' +  parseFloat(formatPercent.formatValue(dt.getValue(row, 1) / (dt.getValue(row, 1) + dt.getValue(row, 2) + dt.getValue(row, 3)))).toFixed(0) + ')';
 						},
 						sourceColumn: 1,
 						type: "string",
 						role: "annotation", },
 					2, {
 						calc: function (dt, row) {
-							return dt.getValue(row, 2) + ' (' + formatPercent.formatValue(dt.getValue(row, 2) / (dt.getValue(row, 1) + dt.getValue(row, 2) + dt.getValue(row, 3))) + ')';
+							return dt.getValue(row, 2) + ' (' +  parseFloat(formatPercent.formatValue(dt.getValue(row, 2) / (dt.getValue(row, 1) + dt.getValue(row, 2) + dt.getValue(row, 3)))).toFixed(0) + ')';
 						},
 						sourceColumn: 2,
 						type: "string",
 						role: "annotation"},
 					3, {
 						calc: function (dt, row) {
-							return dt.getValue(row, 3) + ' (' + formatPercent.formatValue(dt.getValue(row, 3) / (dt.getValue(row, 1) + dt.getValue(row, 2) + dt.getValue(row, 3))) + ')';
+							return dt.getValue(row, 3) + ' (' +  parseFloat(formatPercent.formatValue(dt.getValue(row, 3) / (dt.getValue(row, 1) + dt.getValue(row, 2) + dt.getValue(row, 3)))).toFixed(0) + ')';
 						},
 						sourceColumn: 3,
 						type: "string",
