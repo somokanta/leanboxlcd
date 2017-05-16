84ab0f10e09698d296c72c68f671bf04a9f71c9a
dhirendra mishra <dhirenmishra10@gmail.com>
2017-05-16 18:33:46 +0530

    otif trends

--- sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_otif_trends_chart.js
+++ sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_otif_trends_chart.js
@@ -9,13 +9,13 @@
 	Drupal.behaviors.leanbox_dashboard_otif_trends = {
 		attach: function (context, settings) {
 			
-			var otif = Drupal.settings.leanbox_dashboard.otif;
+			var otif = Drupal.settings.leanbox_dashboard.otif_trends;
       google.charts.load('current', {'packages':['gauge','corechart', 'bar']});
 			google.charts.setOnLoadCallback(function () {
 				drawotifChart(otif);
 			});
 
-			$(".otif-submit").click(function (e) {
+			$(".otif-trends-submit").click(function (e) {
 
 				e.preventDefault();
 			  var start_date = $(this).parent().prev().prev().find("input[name='start_date[date]']").val();
@@ -25,7 +25,7 @@
 						type: "POST",
 						cache: false, //for Chrome and IE8
 						url: "/chart-daterange-filter",
-						data: {start_date: start_date, end_date: end_date, activity_type: 'otif'},
+						data: {start_date: start_date, end_date: end_date, activity_type: 'otif_trends'},
 						success: function (response) {
 							google.charts.setOnLoadCallback(function () {
 								drawotifChart(response.output);
@@ -50,7 +50,7 @@
         };
 
        // var chart = new google.visualization.Gauge(document.getElementById('otif_div'));
-				var chart = new google.visualization.LineChart(document.getElementById('otif_div'));
+				var chart = new google.visualization.LineChart(document.getElementById('otif_trends_div'));
 
         chart.draw(data, options);
 				
