ebd9d99c9ddb7030b0aa133b54ef9d691a95924a
dhirendra mishra <dhirenmishra10@gmail.com>
2017-05-18 12:25:21 +0530

    otif trends new logic change

--- sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_otif_trends_chart.js
+++ sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_otif_trends_chart.js
@@ -8,14 +8,6 @@
 
 	Drupal.behaviors.leanbox_dashboard_otif_trends = {
 		attach: function (context, settings) {
-			
-			var otif = Drupal.settings.leanbox_dashboard.otif_trends;
-			var area_definition = Drupal.settings.leanbox_dashboard.area_definition;
-			console.log(otif);
-      google.charts.load('current', {'packages':['gauge','corechart', 'bar']});
-			google.charts.setOnLoadCallback(function () {
-				drawotifChart(otif, area_definition);
-			});
 
 			$(".otif-trends-submit").click(function (e) {
 
@@ -29,37 +21,12 @@
 						url: "/chart-daterange-filter",
 						data: {start_date: start_date, end_date: end_date, activity_type: 'otif_trends'},
 						success: function (response) {
-							var area_definition = Drupal.settings.leanbox_dashboard.area_definition;
-							
-							google.charts.setOnLoadCallback(function () {
-								drawotifChart(response.output, area_definition);
-							});
-							$('#otiftable').html(response.table);
+							$('#otiftable').html(response);
 						},
 					});
 				}
 				
 			});
-
-			function drawotifChart(otif, area_definition) {
-
-        var data = google.visualization.arrayToDataTable(otif);
-
-        var options = {
-					width: area_definition.width,
-					height: area_definition.height,
-					chartArea: {left: area_definition.ch_left, top: area_definition.ch_top, width: area_definition.ch_width, height: area_definition.ch_height},
-					curveType: 'function',
-          title: '',
-					legend: {position: 'top', maxLines: 3},
-        };
-
-       // var chart = new google.visualization.Gauge(document.getElementById('otif_div'));
-				var chart = new google.visualization.LineChart(document.getElementById('otif_trends_div'));
-
-        chart.draw(data, options);
-				
-			}
 		}
 	};
 })(jQuery);
