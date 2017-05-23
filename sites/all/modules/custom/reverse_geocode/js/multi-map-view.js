772f425fe00d4ee873910935926b4f6855231ac5
Hardik_Patel_12 <hardik.patel@iksula.com>
2017-05-23 14:52:34 +0530

    area hook wise view

--- sites/all/modules/custom/reverse_geocode/js/multi-map-view.js
+++ sites/all/modules/custom/reverse_geocode/js/multi-map-view.js
@@ -64,6 +64,7 @@ var data;
                         var text = result;
                         var mk = addMarker(postion, icon, text);
                         marker[area_hook] = {mk: mk};
+                        marker[area_hook].push({mk : mk});
                         if (checked_lat > max_lat || max_lat == undefined) {
                             max_lat = checked_lat;
                         }
