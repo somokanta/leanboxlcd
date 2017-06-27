b0c7bb6e811cc43b67a72110dd0a92810d84ffe9
Hardik_Patel_12 <hardik.patel@iksula.com>
2017-06-27 15:37:21 +0530

    testing

--- sites/all/modules/custom/reverse_geocode/js/reverse_geocode_incremental_outlet.js
+++ sites/all/modules/custom/reverse_geocode/js/reverse_geocode_incremental_outlet.js
@@ -2,15 +2,15 @@
     Drupal.behaviors.incremental = {
         attach: function (context, settings) {
              console.log("123");
-            $('#incremental-outlet-confirm-data').hide();
-            $('#incremental-outlet-confirm').click(function () {
-                console.log("check clickeddd");
-                $('#incremental-outlet-confirm-data').show();
-            });
-            $('input[type="radio"]').click(function() {
-                console.log("radion clickeddd");
-                $('#incremental-outlet-confirm-data').hide();
-            });
+//            $('#incremental-outlet-confirm-data').hide();
+//            $('#incremental-outlet-confirm').click(function () {
+//                console.log("check clickeddd");
+//                $('#incremental-outlet-confirm-data').show();
+//            });
+//            $('input[type="radio"]').click(function() {
+//                console.log("radion clickeddd");
+//                $('#incremental-outlet-confirm-data').hide();
+//            });
         }
 
     };
