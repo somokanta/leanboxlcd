dc65f510754dce7aa67cab64087ace68dd34b023
Hardik_Patel_12 <hardik.patel@iksula.com>
2017-07-28 12:24:59 +0530

    menus

--- sites/all/themes/leanbox/templates/block--superfish--4.tpl.php
+++ sites/all/themes/leanbox/templates/block--superfish--4.tpl.php
@@ -5,7 +5,7 @@
  * and open the template in the editor.
  */
 ?>  
-<div class="menu_overlay"></div> 
+<div class="menu_overlay"></div>
 <div id='collapsable' class='expanded menu_wrapper'><div class="explore_menu">Explore</div>
     <div id="block-<?php print $block->module . '-' . $block->delta; ?>" class="menu_dropdown <?php print $classes; ?> " <?php print $attributes; ?> >
         <?php print render($title_prefix); ?>
