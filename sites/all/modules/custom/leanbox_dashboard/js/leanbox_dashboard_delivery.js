58d3acc0f3b99de312cfc5ce922947faedb95471
dhirendra mishra <dhirenmishra10@gmail.com>
2017-08-09 11:53:22 +0530

    all

--- sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_delivery.js
+++ sites/all/modules/custom/leanbox_dashboard/js/leanbox_dashboard_delivery.js
@@ -57,7 +57,9 @@
 					bar: {groupWidth: '40%'},
 					chartArea: {left: area_definition.ch_left, top: area_definition.ch_top, width: area_definition.ch_width, height: area_definition.ch_height},
 					series: {
-						2: {color: '#ff0000'},
+							0: {color: '#ec3c10'},
+						1: {color: '#27ae60'},
+						2: {color: 'red'},
 					},
 					vAxis: {
 						minValue: 0,
