7252556f0bae50d635e8764010bbebd2b55df90d
dhirendra mishra <dhirenmishra10@gmail.com>
2017-08-10 15:47:44 +0530

    al

--- sites/all/modules/custom/leanbox_mobile/js/leanbox_mobile_returnchart.js
+++ sites/all/modules/custom/leanbox_mobile/js/leanbox_mobile_returnchart.js
@@ -51,7 +51,7 @@
 					title: '',
 					is3D: true,
           sliceVisibilityThreshold: 0,
-					chartArea:{left:30,top:20,width:'100%',height:'75%'},
+					chartArea:{left:30,top:20,width:'100%',height:'100%'},
 					legend: {position: 'right'},
 				};
 				var chart = new google.visualization.PieChart(document.getElementById("mobile_return_div"));
