159f8a1a7a25b0d0b160186ce28f7b9eafccd13c
abhishek <abhishek.r@iksula.com>
2017-08-23 19:27:29 +0530

    abhishek

--- sites/all/themes/leanbox/js/leanbox_custom.js
+++ sites/all/themes/leanbox/js/leanbox_custom.js
@@ -1,20 +1,20 @@
-//jQuery(document).ready(function($){
-//    var find_date_picker = $(".date-padding").length;
-//    //alert(find_date_picker);
-//
-//    if (find_date_picker) {
-//    	$(".date-padding").closest(".views-exposed-widget").addClass("date_field_parent");
-//    }
-//
-//    jQuery(".explore_menu").click(function(){
-//    	$(".menu_overlay").toggle();
-//    	$(".menu_dropdown").stop(true,true).slideToggle();
-//        jQuery('li.expanded.dropdown a').removeAttr('data-toggle');
-//    });
-//
-//     $(".menu_overlay").click(function(){
-//    	$(".menu_overlay").toggle();
-//    	$(".menu_dropdown").stop(true,true).slideUp();
-//    });
-//
-//});
+jQuery(document).ready(function($){
+    var find_date_picker = $(".date-padding").length;
+    //alert(find_date_picker);
+
+    if (find_date_picker) {
+    	$(".date-padding").closest(".views-exposed-widget").addClass("date_field_parent");
+    }
+
+    jQuery(".explore_menu").click(function(){
+    	$(".menu_overlay").toggle();
+    	$(".menu_dropdown").stop(true,true).slideToggle();
+        jQuery('li.expanded.dropdown a').removeAttr('data-toggle');
+    });
+
+     $(".menu_overlay").click(function(){
+    	$(".menu_overlay").toggle();
+    	$(".menu_dropdown").stop(true,true).slideUp();
+    });
+
+});
