16dd3e1e7e72a19bc0f7653df0ee4549cd483427
abhishek <abhishek.r@iksula.com>
2017-09-19 18:00:04 +0530

    all

---
+++ test1.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * SAP PI Push to SAP Multithreading
+ */
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+define('DRUPAL_ROOT', getcwd());
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+
+dsm($_SERVER['argv'][1]);
+dsm("hello");
+
+?>
