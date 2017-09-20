adcbe5877b6aebcd93e49ef5396c961fdc125d40
abhishek <abhishek.r@iksula.com>
2017-09-20 10:45:14 +0530

    original code

--- test1.php
+++ test1.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * SAP PI Push to SAP Multithreading
+ * Cron bill creation
  */
 
 error_reporting(E_ALL);
@@ -12,8 +12,21 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
+	dsm($_SERVER['argv'][1]);
+ 	global $user;
+ 	$user_load = user_load($_SERVER['argv'][1]);
+ 	$user = $user_load;
+ 	user_login_finalize();
+	
+	dsm("hello");
+	dsm($user);
 
-dsm($_SERVER['argv'][1]);
-dsm("hello");
-watchdog('mine', "helloooooo".$_SERVER['argv'][1]);
+	watchdog('cron_test', "helloooooo".$_SERVER['argv'][1]);
+	watchdog('cron_test', '<pre>' . print_r($user, TRUE) . '</pre>');
+
+	if (user_is_logged_in()) {
+		module_load_include('pages.inc', 'user', 'user');
+		user_logout_current_user();
+	}
+
 ?>
