d2acd7ffe5e4ca7190fd6e1894c77e391c4d0b0f
abhishek <abhishek.r@iksula.com>
2017-10-04 17:49:09 +0530

    abhi

--- sites/all/modules/custom/change_feeds_import/js/prevent_multiple_click.js
+++ sites/all/modules/custom/change_feeds_import/js/prevent_multiple_click.js
@@ -2,11 +2,8 @@
     Drupal.behaviors.Reclicks = {
         attach: function (context, settings) {
             jQuery(".multiple_click a").click(function () {
-                alert("called0");
                 jQuery(this).click(function (e) {
-                    alert("called1");
                     e.preventDefault();
-                    alert("called2");
                 });
                 return true;
             });
