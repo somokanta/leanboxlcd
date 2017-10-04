9d94910a8abde40c8d1f0ed9617c720f2a7c667b
abhishek <abhishek.r@iksula.com>
2017-10-04 17:51:21 +0530

    billing data else

---
+++ sites/all/modules/custom/change_feeds_import/js/prevent_multiple_click.js
@@ -0,0 +1,12 @@
+(function ($) {
+    Drupal.behaviors.Reclicks = {
+        attach: function (context, settings) {
+            jQuery(".multiple_click a").click(function () {
+                jQuery(this).click(function (e) {
+                    e.preventDefault();
+                });
+                return true;
+            });
+        }
+    };
+})(jQuery);
