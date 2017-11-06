d7f074fdaff3151e14050438f538391f2d83b538
Shruti <shruti.a@iksula.com>
2017-11-06 20:00:14 +0530

    add js and validation

--- sites/all/modules/custom/leanbox_payment_collection/js/leanbox_payment_collection.js
+++ sites/all/modules/custom/leanbox_payment_collection/js/leanbox_payment_collection.js
@@ -2,8 +2,8 @@
 	Drupal.behaviors.leanbox_payment_collection = {
 		attach: function (context) {
       $('.field-name-field-field-amnt-difference input').attr("readonly", true);
-      $(".field-name-field-cash-amt input").change(function() {
-        var cash_amt = $(this).val();
+      $(".field-name-field-cash-amt , .field-name-field-collection-cheque-amt input").change(function() {
+        //var cash_amt = $(this).val();
         var id = $(this).attr('id');
         var res = id.split('-');
         var nid = res[2];
@@ -11,12 +11,12 @@
         var bill_value = $('.bill_value-'+nid).text();     
 
         var prev_cash_amt = $('.prev_cash_amt-'+nid).text();
-        if(prev_cash_amt == '') {
+        if(prev_cash_amt == NaN) {
           prev_cash_amt = 0;
         }
 
         var prev_chq_amt = $('.prev_ch_amt-'+nid).text();
-        if(prev_chq_amt == '') {
+        if(prev_chq_amt == NaN) {
           prev_chq_amt = 0;
         }
         
@@ -24,39 +24,16 @@
         if(ch_amt == '') {
           ch_amt = 0;
         }
-
-        var total = parseInt(bill_value) - (parseInt(cash_amt) + parseInt(ch_amt) + parseInt(prev_cash_amt) + parseInt(prev_chq_amt));
-        
-        $('#edit-node-'+nid+'-field-field-amnt-difference-und-0-value').val(total);
-      });
-      
-      $(".field-name-field-collection-cheque-amt input").change(function() {
-        var chq_amt = $(this).val();
-        var id = $(this).attr('id');
-        var res = id.split('-');
-        var nid = res[2];
-
-        var bill_value = $('.bill_value-'+nid).text();
-
-        var prev_cash_amt = $('.prev_cash_amt-'+nid).text();
-        if(prev_cash_amt == '') {
-          prev_cash_amt = 0;
-        }
-
-        var prev_chq_amt = $('.prev_ch_amt-'+nid).text();
-        if(prev_chq_amt == '') {
-          prev_chq_amt = 0;
-        }
-        
         var cash_amt = $('#edit-node-'+nid+'-field-cash-amt-und-0-value').val();
         if(cash_amt == '') {
           cash_amt = 0;
         }
 
-        var total = parseInt(bill_value) - (parseInt(cash_amt) +  parseInt(chq_amt) + parseInt(prev_cash_amt) + parseInt(prev_chq_amt));
-      
+        var total = parseInt(bill_value) - (parseInt(cash_amt) + parseInt(ch_amt) + parseInt(prev_cash_amt) + parseInt(prev_chq_amt));
+        
         $('#edit-node-'+nid+'-field-field-amnt-difference-und-0-value').val(total);
-      });   
+      });
+       
   	}
 	};
 })(jQuery);
