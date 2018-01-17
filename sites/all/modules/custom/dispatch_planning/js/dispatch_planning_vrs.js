762ed6728a5c21eed5ca01cab8e11bf80ace3327
dhiren <dhirenmishra10@gmail.com>
2018-01-17 16:05:21 +0530

    all

---
+++ sites/all/modules/custom/dispatch_planning/js/dispatch_planning_vrs.js
@@ -0,0 +1,18 @@
+(function ($) {
+    Drupal.behaviors.vrs = {
+        attach: function (context, settings) {
+//            jQuery(".multiple_click a").click(function () {
+//                if(jQuery("#referesh_warning").length == 1) {
+//                    jQuery("#referesh_warning").show();
+//                }
+//                jQuery(this).click(function (e) {
+//                    //e.preventDefault();
+//                    return false;
+//                });
+//                return true;
+//            });
+
+
+        }
+    };
+})(jQuery);
