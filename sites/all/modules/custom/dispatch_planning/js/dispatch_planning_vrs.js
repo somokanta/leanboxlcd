695f161378ea5e5088b6484e75e16d6e16b9161c
dhiren <dhirenmishra10@gmail.com>
2018-01-18 21:03:06 +0530

    all

--- sites/all/modules/custom/dispatch_planning/js/dispatch_planning_vrs.js
+++ sites/all/modules/custom/dispatch_planning/js/dispatch_planning_vrs.js
@@ -12,8 +12,8 @@
             var total_debit = 0;
 
             $(".Signed_Bills-collected").change(function () {
-                var signed_app = $('.Signed_Bills-app_signed_bills-val').attr('data-value');
-                var signed_collected = $('.Signed_Bills-collected').val();
+                var signed_app = parseFloat($('.Signed_Bills-app_signed_bills-val').attr('data-value'));
+                var signed_collected = parseFloat($('.Signed_Bills-collected').val());
                 signed_difference = signed_app - signed_collected;
                 $('.Signed_Bills-difference').val(signed_difference);
                 total = signed_difference + cheque_difference + cash_difference;
@@ -30,8 +30,8 @@
 
 
             $(".Cheque_Bills-collected").change(function () {
-                var cheque_app = $('.Cheque_Bills-app_cheque_bills-val').attr('data-value');
-                var cheque_collected = $('.Cheque_Bills-collected').val();
+                var cheque_app = parseFloat($('.Cheque_Bills-app_cheque_bills-val').attr('data-value'));
+                var cheque_collected = parseFloat($('.Cheque_Bills-collected').val());
                 cheque_difference = cheque_app - cheque_collected;
                 $('.Cheque_Bills-difference').val(cheque_difference);
                 total = signed_difference + cheque_difference + cash_difference;
@@ -48,8 +48,8 @@
 
 
             $(".Cash_Bills-collected").change(function () {
-                var cash_app = $('.Cash_Bills-app_cash_bills-val').attr('data-value');
-                var cash_collected = $('.Cash_Bills-collected').val();
+                var cash_app = parseFloat($('.Cash_Bills-app_cash_bills-val').attr('data-value'));
+                var cash_collected = parseFloat($('.Cash_Bills-collected').val());
                 cash_difference = cash_app - cash_collected;
                 $('.Cash_Bills-difference').val(cash_difference);
                 total = signed_difference + cheque_difference + cash_difference;
@@ -66,8 +66,8 @@
             });
 
             $(".ret_collected").change(function () {
-                var ret_app = $('.ret_val').attr('data-value');
-                var ret_collected = $('.ret_collected').val();
+                var ret_app = parseFloat($('.ret_val').attr('data-value'));
+                var ret_collected = parseFloat($('.ret_collected').val());
                 ret_difference = ret_app - ret_collected;
                 $('.ret_difference').val(ret_difference);
                 $('.inventory_detail').val(ret_difference);
@@ -78,10 +78,11 @@
 
 
             function _calculate_total_debit() {
-                van_bill_value = $('.van_bill_value').val();
-                cash_debit = $('.cash_debit').val();
-                inventory_detail = $('.inventory_detail').val();
+                van_bill_value = parseFloat($('.van_bill_value').val());
+                cash_debit = parseFloat($('.cash_debit').val());
+                inventory_detail = parseFloat($('.inventory_detail').val());
                 total_debit = van_bill_value - (cash_debit + inventory_detail);
+
                 $('.total_debit').val(total_debit);
             }
 
