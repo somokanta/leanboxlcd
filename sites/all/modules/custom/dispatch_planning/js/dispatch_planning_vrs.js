39a29c329ad8e511c14b495a1ca794cb67e654b7
dhiren <dhirenmishra10@gmail.com>
2018-01-19 15:46:14 +0530

    all

--- sites/all/modules/custom/dispatch_planning/js/dispatch_planning_vrs.js
+++ sites/all/modules/custom/dispatch_planning/js/dispatch_planning_vrs.js
@@ -2,9 +2,9 @@
     Drupal.behaviors.vrs = {
         attach: function (context, settings) {
             var total = 0;
-            var signed_difference = 0;
-            var cheque_difference = 0;
-            var cash_difference = 0;
+            var signed_collected = 0;
+            var cheque_collected = 0;
+            var cash_collected = 0;
             var ret_difference = 0;
             var van_bill_value = 0;
             var cash_debit = 0;
@@ -12,11 +12,11 @@
             var total_debit = 0;
 
             $(".Signed_Bills-collected").change(function () {
-                var signed_app = parseFloat($('.Signed_Bills-app_signed_bills-val').attr('data-value'));
-                var signed_collected = parseFloat($('.Signed_Bills-collected').val());
-                signed_difference = signed_app - signed_collected;
+                var signed_Sum = parseFloat($('.Signed_Bills-Sum_signed_bills-val').attr('data-value'));
+                signed_collected = parseFloat($('.Signed_Bills-collected').val());
+                var signed_difference = signed_Sum - signed_collected;
                 $('.Signed_Bills-difference').val(signed_difference);
-                total = signed_difference + cheque_difference + cash_difference;
+                total = signed_collected + cheque_collected + cash_collected;
                 if (total == NaN) {
                     total = '';
                 }
@@ -30,11 +30,11 @@
 
 
             $(".Cheque_Bills-collected").change(function () {
-                var cheque_app = parseFloat($('.Cheque_Bills-app_cheque_bills-val').attr('data-value'));
-                var cheque_collected = parseFloat($('.Cheque_Bills-collected').val());
-                cheque_difference = cheque_app - cheque_collected;
+                var cheque_Sum = parseFloat($('.Cheque_Bills-Sum_cheque_bills-val').attr('data-value'));
+                cheque_collected = parseFloat($('.Cheque_Bills-collected').val());
+                var cheque_difference = cheque_Sum - cheque_collected;
                 $('.Cheque_Bills-difference').val(cheque_difference);
-                total = signed_difference + cheque_difference + cash_difference;
+                total = signed_collected + cheque_collected + cash_collected;
                 if (total == NaN) {
                     total = '';
                 }
@@ -48,11 +48,11 @@
 
 
             $(".Cash_Bills-collected").change(function () {
-                var cash_app = parseFloat($('.Cash_Bills-app_cash_bills-val').attr('data-value'));
-                var cash_collected = parseFloat($('.Cash_Bills-collected').val());
-                cash_difference = cash_app - cash_collected;
+                var cash_Sum = parseFloat($('.Cash_Bills-Sum_cash_bills-val').attr('data-value'));
+                cash_collected = parseFloat($('.Cash_Bills-collected').val());
+                var cash_difference = cash_Sum - cash_collected;
                 $('.Cash_Bills-difference').val(cash_difference);
-                total = signed_difference + cheque_difference + cash_difference;
+                total = signed_collected + cheque_collected + cash_collected;
 
                 if (total == NaN) {
                     total = '';
@@ -70,7 +70,7 @@
                 var ret_collected = parseFloat($('.ret_collected').val());
                 ret_difference = ret_app - ret_collected;
                 $('.ret_difference').val(ret_difference);
-                $('.inventory_detail').val(ret_difference);
+                $('.inventory_detail').val(ret_collected);
                 
                 //
                 _calculate_total_debit();
