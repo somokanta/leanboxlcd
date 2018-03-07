b20a8d1ba21fa9fd78e1077da687e1667e0a75c9
abhishek <abhishek.r@iksula.com>
2018-03-07 17:06:07 +0530

    abhi

--- sites/all/themes/bootstrap/templates/system/status-messages.func.php
+++ sites/all/themes/bootstrap/templates/system/status-messages.func.php
@@ -60,18 +60,18 @@ function bootstrap_status_messages($variables) {
     $output .= "  <a class=\"close\" data-dismiss=\"alert\" href=\"#\">&times;</a>\n";
 
     if (!empty($status_heading[$type])) {
-      $output .= '<h4 class="element-invisible">' . _bootstrap_filter_xss($status_heading[$type]) . "</h4>\n";
+      $output .= '<h4 class="element-invisible">' . ($status_heading[$type]) . "</h4>\n";
     }
 
     if (count($messages) > 1) {
       $output .= " <ul>\n";
       foreach ($messages as $message) {
-        $output .= '  <li>' . _bootstrap_filter_xss($message) . "</li>\n";
+        $output .= '  <li>' . ($message) . "</li>\n";
       }
       $output .= " </ul>\n";
     }
     else {
-      $output .= _bootstrap_filter_xss($messages[0]);
+      $output .= ($messages[0]);
     }
 
     $output .= "</div>\n";
