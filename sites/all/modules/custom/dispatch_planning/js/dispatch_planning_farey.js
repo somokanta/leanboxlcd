d3e51642b0ac9128aabec2bbcebf54f0600219af
dhiren <dhirenmishra10@gmail.com>
2018-03-15 16:39:04 +0530

    all

--- sites/all/modules/custom/dispatch_planning/js/dispatch_planning_farey.js
+++ sites/all/modules/custom/dispatch_planning/js/dispatch_planning_farey.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.farey = {
     attach: function (context, settings) {
       $("div.error-msg").hide();
-      $('#edit-submit').show();
+      $(".form-submit").show();
       $(".sku_reason_list").prop('disabled', 'disabled');
 
       // Check Order Status partial_deliver  
@@ -12,15 +12,15 @@
         $(".actual_qty_value").on('change', function () {
           var actual_qty = $(this).val();
           var net_qty = $(this).closest('td').prev().attr('data-value');
-          if (actual_qty > net_qty) {
+          if (actual_qty >= net_qty) {
             $("div.error-msg").show();
             $("div.error-msg").css({'color': 'red', 'font-size': '100%'});
-            $('#edit-submit').hide();
+            $(".form-submit").hide();
             $(this).parent().parent().next().find('select').prop('disabled', 'disabled');
           }
           else {
             $("div.error-msg").hide();
-            $('#edit-submit').show();
+            $(".form-submit").show();
             $(this).parent().parent('td').next().find('select').removeAttr('disabled');
           }
 
