0d761a9c387a9823aecaae3aca557f6172d0aed2
abhishek ray <abhishek.r@iksula.com>
2019-08-20 16:53:56 +0530

    abhi

--- sites/all/modules/custom/prevent_multiple_click/js/prevent_multiple_form_click.js
+++ sites/all/modules/custom/prevent_multiple_click/js/prevent_multiple_form_click.js
@@ -1,12 +1,23 @@
-(function ($) {
-    Drupal.behaviors.Reclicks = {
-        attach: function (context, settings) {
-            jQuery("form").submit(function () {
-                jQuery(this).submit(function (e) {
-                    e.preventDefault();
-                });
-                return true;
-            });
-        }
-    };
-})(jQuery);
+Drupal.behaviors.formSingleSubmit = {
+   attach: function() {
+       function onFormSubmit(e) {
+           if (e.isDefaultPrevented()) {
+               // Don't act on form submissions that have been prevented by other JS.
+               return;
+           }
+           var $form = jQuery(e.currentTarget);
+           var formValues = $form.serialize();
+           var previousValues = $form.attr('data-drupal-form-submit-last');
+           if (previousValues === formValues) {
+               e.preventDefault();
+           }
+           else {
+               $form.attr('data-drupal-form-submit-last', formValues);
+           }
+       }
+
+       jQuery('body').once('form-single-submit')
+               .delegate('form:not([method~="GET"])', 'submit.singleSubmit', onFormSubmit);
+
+   }
+};
