bb09ef2714d0717ae12d4fd0d84887ab471c35e9
Sujit Shelke <sujit.iksula@gmail.com>
2020-01-16 11:08:25 +0530

    change

--- sites/all/modules/custom/order_packing_slip/js/pdf_print.js
+++ sites/all/modules/custom/order_packing_slip/js/pdf_print.js
@@ -1,7 +1,8 @@
 (function ($) {
   Drupal.behaviors.hours = {
     attach: function (context, settings) {
-        //alert('hi');
+        alert('hi');
+        console.log('hi');
         //$(window).load(function(){ alert('hi'); });
     }
   };
