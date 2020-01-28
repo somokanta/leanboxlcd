338589c263cc6084b8c7a9af1e8c28aef9eab97a
Umesh Sapariya <umeshsapariya@gmail.com>
2020-01-28 14:59:41 +0530

    changes

---
+++ sites/all/modules/custom/order_packing_slip/js/pdf_print.js
@@ -0,0 +1,9 @@
+(function ($) {
+  Drupal.behaviors.hours = {
+    attach: function (context, settings) {
+        alert('hi');
+        console.log('hi');
+        //$(window).load(function(){ alert('hi'); });
+    }
+  };
+})(jQuery);
