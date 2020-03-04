366d939e23707b3fac2c3dbfdd1e968f195ccf8b
Adil Siddiqui <adil.s@iksula.com>
2020-03-04 15:20:50 +0530

    changes

--- sites/all/modules/custom/mrc_inventory_returns/js/upload_signed_bill_cheque.js
+++ sites/all/modules/custom/mrc_inventory_returns/js/upload_signed_bill_cheque.js
@@ -1,13 +1,16 @@
 (function ($) {
     Drupal.behaviors.upload_signed_bill_cheque = {
         attach: function (context, settings) {
+            var billtable,billtr,billtd,pay_outcome,signtd,cheqtd;
             if ($('.error').is(':visible')) {
                 bill_status_undeliver_filter();
             }
+            $("#bill_status_undeliver").hide();
 
             $("#bill_number").keyup(function () {
                 bill_filter();
             });
+            payment_outcome_filter();
             $("#payment_outcome").on('change', function () {
                 payment_outcome_filter();
             });
@@ -21,6 +24,38 @@
             $("#bill_status_undeliver").on('change', function () {
                 bill_status_undeliver_filter();
             });
+            
+            // show hide based on payment outcome
+            billtable = document.getElementById("bill_details_one");
+            if (billtable != null) {
+                billtr = billtable.getElementsByTagName("tr");
+                if (billtr != null) {
+                    for (i = 0; i < billtr.length; i++) {
+                        billtd = billtr[i].getElementsByTagName("td")[3];
+                        if (billtd != null) {
+                            pay_outcome = billtd.innerText;
+                            if (pay_outcome == 'Cash') {
+                                signtd = billtr[i].getElementsByTagName("td")[4].style.visibility = 'hidden';
+                                cheqtd = billtr[i].getElementsByTagName("td")[5].style.visibility = 'hidden';
+                            }
+                            if (pay_outcome == 'Signed') {
+                                //signtd = billtr[i].getElementsByTagName("td")[4].style.visibility = 'hidden';
+                                cheqtd = billtr[i].getElementsByTagName("td")[5].style.visibility = 'hidden';
+                            }
+                            if (pay_outcome == 'Cheque,Cash') {
+                                signtd = billtr[i].getElementsByTagName("td")[4].style.visibility = 'hidden';
+                                //cheqtd = billtr[i].getElementsByTagName("td")[5].style.visibility = 'hidden';
+                            }
+                            if (pay_outcome == 'Cheque') {
+                                signtd = billtr[i].getElementsByTagName("td")[4].style.visibility = 'hidden';
+                                //cheqtd = billtr[i].getElementsByTagName("td")[5].style.visibility = 'hidden';
+                            }                             
+                        }
+                    }
+                }
+
+            }
+            //show hide based on payment outcome
 
 
             // Reseting filters
@@ -48,7 +83,8 @@
 
                 filter = input.value.toUpperCase();
                 table = document.getElementById("bill_details_one");
-                tr = table.getElementsByTagName("tr");
+                if(table != null){
+                                    tr = table.getElementsByTagName("tr");
                 for (i = 0; i < tr.length; i++) {
                     td = tr[i].getElementsByTagName("td")[0];
                     if (td) {
@@ -60,6 +96,8 @@
                     }
                 }
                 bill_status_undeliver_filter();
+                }
+
             }
             function payment_outcome_filter() {
                 var input, filter, table, tr, td, i;
@@ -71,18 +109,21 @@
                 $("#bill_number").val('');
                 filter = input.value.toUpperCase();
                 table = document.getElementById("bill_details_one");
-                tr = table.getElementsByTagName("tr");
-                for (i = 0; i < tr.length; i++) {
-                    td = tr[i].getElementsByTagName("td")[3];
-                    if (td) {
-                        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                            tr[i].style.display = "";
-                        } else {
-                            tr[i].style.display = "none";
+                if(table != null){
+                    tr = table.getElementsByTagName("tr");
+                    for (i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td")[3];
+                        if (td) {
+                            if (td.innerHTML.toUpperCase() == filter) {
+                                tr[i].style.display = "";
+                            } else {
+                                tr[i].style.display = "none";
+                            }
                         }
                     }
+                    bill_status_undeliver_filter();                    
                 }
-                bill_status_undeliver_filter();
+
             }
 
             function bill_status_filter() {
@@ -92,6 +133,7 @@
                 //var select = $('#bank_name');
                 //select.val($('options:first', select).val());
                 $("#bank_name").val('');
+                $("#payment_outcome").val('');
 
                 $("#bill_status_undeliver").prop('checked', false);
                 if (input == 'deliver') {
@@ -105,24 +147,27 @@
                 }
 
                 table = document.getElementById("bill_details_one");
-                tr = table.getElementsByTagName("tr");
-                for (i = 0; i < tr.length; i++) {
-                    td = tr[i].getElementsByTagName("td")[1];
-                    if (td) {
-                        if (check_all == 0) {
-                            if (td.innerHTML.indexOf(filter) === 0) {
-                                tr[i].style.display = "";
+                if(table != null){
+                    tr = table.getElementsByTagName("tr");
+                    for (i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td")[1];
+                        if (td) {
+                            if (check_all == 0) {
+                                if (td.innerHTML.indexOf(filter) === 0) {
+                                    tr[i].style.display = "";
+                                } else {
+                                    tr[i].style.display = "none";
+                                }
                             } else {
-                                tr[i].style.display = "none";
-                            }
-                        } else {
-                            if (td.innerHTML.indexOf('Full Returned') !== 0) {
-                                tr[i].style.display = "";
+                                if (td.innerHTML.indexOf('Full Returned') !== 0) {
+                                    tr[i].style.display = "";
+                                }
                             }
                         }
                     }
+                    bill_status_undeliver_filter();                    
                 }
-                bill_status_undeliver_filter();
+
             }
 
             function bank_name_filter() {
